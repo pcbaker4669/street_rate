6a0b947f78ca5b9ecc435956e26c2533c4261562
Peter Baker <peter.baker4669@yahoo.com>
2025-01-19 08:10:37 -0500

    recommit first commit

---
+++ street_rate_anugi/src/app/map/map.component.ts
@@ -0,0 +1,32 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-map',
+  standalone: false,
+  
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent {
+  @ViewChild('map') mapElement!: ElementRef;
+  map!: Map;
+
+  ngAfterViewInit(): void {
+    this.map = new Map({
+      target: this.mapElement.nativeElement,
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+}
